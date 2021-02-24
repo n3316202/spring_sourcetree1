a5f7acdfceaeb4552f6e19316db40594c541c6d3
bit <admin@admin.com>
2021-02-24 15:53:47 +0900

    Revert "master #5"
    This reverts commit f6b7e4d150285a6fb18fd6b89880523745d6d35b.

--- src/main/java/edu/bi/ex/HomeController.java
+++ src/main/java/edu/bi/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#5 수정
 //#3 수정
 //#2 수정
 @Controller
