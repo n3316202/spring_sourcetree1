ecd2e2c0386ce7956c8a7ba19b8eae98226b0ecd
bit <admin@admin.com>
2021-02-24 15:54:54 +0900

    Revert "master #3"
    This reverts commit ec3518595320b887152512f515ac2148b085f900.

--- src/main/java/edu/bi/ex/HomeController.java
+++ src/main/java/edu/bi/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#3 수정
 //#2 수정
 @Controller
 public class HomeController {
